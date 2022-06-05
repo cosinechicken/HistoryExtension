0028fd3d342027fcfc14811174cdd97dc1ad9d88
cosinechicken <ah.blenden@gmail.com>
2022-06-05 14:49:45 -0400

    Added active tab highlighting
    Kind of messy though.

--- getting-started/options/nav.js
+++ getting-started/options/nav.js
@@ -1,8 +1,15 @@
+pagelist = ["home", "stats"]
 fetch('nav.html')
 .then(res => res.text())
 .then(text => {
+    pagelist.forEach(page => text = text.replace("class=\"tablink active\" id=\"" + page +"\"", "class=\"tablink\" id=\"" + page +"\""));
+    // get the current page
+    let urlArr = document.URL.split("/");
+    let pageName = urlArr[urlArr.length - 1].split(".")[0];
+    text = text.replace("class=\"tablink\" id=\"" + pageName +"\"", "class=\"tablink active\" id=\"" + pageName +"\"");
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
+
